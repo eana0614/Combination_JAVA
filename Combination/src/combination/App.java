9165f728e70c2435e8e94879dd574c0db24df198
eana0614 <eana0614@DESKTOP-C7AKB6O>
2017-04-25 17:41:49 +0900

    Modify combination function

--- Combination/src/combination/App.java
+++ Combination/src/combination/App.java
@@ -1,10 +1,62 @@
 package combination;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("!] Input Different Numbers or characters. ");
+		String input = scanner.nextLine();
+		
+		if(checkingString(input)){
+		
+			System.out.println("!] Input N and R. ex) 5 4 ");
+			int n = scanner.nextInt();
+			int r = scanner.nextInt();
+			scanner.nextLine();
+			
+			if(checkingInt(input.length(), n, r)){
+				
+				char[] elements = input.toCharArray();
+				
+				Combination combination = new Combination(r);
+				
+				combination.runCombination(elements, n, r, r);
+				combination.printCombination();
+				
+				System.out.println("!] Done.");
+			}
+			
+		}
+		
+	}
 
+	private static boolean checkingInt(int length, int n, int r) {
+		
+		if(length >= r && length >= n && n >= r){
+			return true;
+		}
+		
+		return false;
+	}
+
+	private static boolean checkingString(String input) {
+		
+		boolean result =  true;
+		
+		for(int i = 1; i<input.length(); i++){
+			char temp = input.charAt(i);
+			for(int j=0; j<i; j++){
+				if(temp == input.charAt(j)){
+					result = false;
+					break;
+				}
+			}
+		}
+		return result;
 	}
 
 }
--- Combination/src/combination/Combination.java
+++ Combination/src/combination/Combination.java
@@ -1,5 +1,51 @@
 package combination;
 
+import java.util.ArrayList;
+
 public class Combination {
+	
+	ArrayList<String> combinationElement;
+	char[] temp;
+	
+	public Combination(int length){
+		combinationElement = new ArrayList<>();
+		temp = new char[length];
+	}
+
+	public void runCombination(char[] elements, int n, int r, int length) {
+		
+		if(r == 0){
+			addCombinationElement(length);
+		}else if(n>=r){
+			
+			temp[r-1] = elements[n-1];
+			runCombination(elements, n-1, r-1, length);
+			runCombination(elements, n-1, r, length);
+			
+		}else{
+			
+		}
+		
+	}
+	
+	private void addCombinationElement(int length) {
+		
+		String t = "";
+		for(int i=length-1; i>= 0; i--){
+			t = t + temp[i];
+		}
+		
+		combinationElement.add(t);
+	}
+
+	public void printCombination(){
+		
+		System.out.println("===== Result =====");
+		for(int i=0; i<combinationElement.size(); i++){
+			System.out.println((i+1)+"th Combination Element = "+combinationElement.get(i));
+		}
+		
+		System.out.println();
+	}
 
 }
